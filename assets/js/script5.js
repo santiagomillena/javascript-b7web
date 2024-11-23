0c4a0bb738235c9ad8c8676c9286626b296f971d
santiagomillena <santiagomillena@gmail.com>
2024-11-23 19:30:51 -0400

    Condicional Dupla (if else)

---
+++ assets/js/script5.js
@@ -0,0 +1,10 @@
+// Condicional Dupla (if else)
+let idade = 18
+
+if (idade > 18){
+    console.log('Você é uma criança.');
+} else if (idade >= 18 && idade < 60) {
+    console.log('Você é um adulto.');
+} else if (idade >= 60) {
+    console.log('Você é um idoso.');
+}
