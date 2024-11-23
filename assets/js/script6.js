9d7a7765e56e50e42f872e40ddbef2532e24ca90
santiagomillena <santiagomillena@gmail.com>
2024-11-23 19:43:54 -0400

    Desmistificando o IF

---
+++ assets/js/script6.js
@@ -0,0 +1,19 @@
+//let ok = true;
+
+let idade = 14;
+
+//let adulto = idade > 18 && idade < 60;
+
+/*if (ok){
+    console.log("Entrou no IF");
+} else {
+    console.log("Não entrou no IF");
+}*/
+
+//if (adulto) {
+//    console.log("Você é adulto")
+//} else {
+//    console.log("Você não é adulto")
+//}
+
+console.log( idade > 18);
