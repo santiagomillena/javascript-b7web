f9a992f1a8d856284d407bf1a1111ceb061c39f0
santiagomillena <santiagomillena@gmail.com>
2024-11-23 17:38:48 -0400

    Criando Váriaveis

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
 console.log('Olá, mundo 3.0')
+
+//Tipos de dados
+
+let nome = "Bonieky" // string
+let idade = 90 // number
+let logado = true // boolean
+let lista = ["ovo", "leite", "trigo"] // array
+let nomeCompleto = {nome: "Bonieky", idade: 90} // object
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof logado)
+console.log(typeof lista)
+console.log(typeof nomeCompleto)
+
+//Criando Váriaveis
+
+let nomeDoCarro = "Ferrari"
+console.log(nomeDoCarro)
+
+let precoDoBolo = 19.99
+console.log(precoDoBolo)
+
+let cidade = "São Paulo"
+console.log(cidade)
+
