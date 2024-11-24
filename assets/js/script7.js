a46c54e268a551ca24cfb9f8da15ae31420ea9b3
santiagomillena <santiagomillena@gmail.com>
2024-11-23 20:07:57 -0400

    exercício de condicionais

---
+++ assets/js/script7.js
@@ -0,0 +1,19 @@
+let x = 10;
+let y = 5;
+console.log(x > y);
+
+let w = "10";
+let z = 10;
+console.log(w === z);
+
+let a = 10;
+let b = 5;
+console.log(x != y);
+
+let preco = 40.3;
+
+if (preco <= 45) {
+    console.log(`R$${preco} está barato!`);
+} else {
+    console.log("O preço da carne está caro!")
+}
