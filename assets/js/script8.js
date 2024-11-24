90194a78e7849141cbadb8f4aa6556f666f9fb99
santiagomillena <santiagomillena@gmail.com>
2024-11-23 20:16:05 -0400

    Condicional Ternário

---
+++ assets/js/script8.js
@@ -0,0 +1,10 @@
+let isMember = true;
+let shipping = isMember ? 2 : 10;
+
+console.log(isMember ? "Você é membro" : "Você não é um membro");
+console.log("Frete: " + shipping);
+
+let age = 90;
+let isAdult = (age >= 18 ? "Yes" : "No");
+
+console.log(isAdult);
