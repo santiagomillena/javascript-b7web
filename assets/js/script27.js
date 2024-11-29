0aab1eb3fe4af8b31911da0124d717cff63130a1
santiagomillena <santiagomillena@gmail.com>
2024-11-29 17:00:20 -0400

    every()

--- assets/js/script27.js
+++ assets/js/script27.js
@@ -6,3 +6,13 @@ let bigFruits = fruits.filter((item) => {
 
 //let bigFruits = fruits.filter((item) => item.length > 4)
 console.log (bigFruits)
+
+let ok = fruits.every((value) =>{
+    return value.length > 3
+})
+
+if (ok){
+    console.log ('Todos são maior que 3')
+} else {
+    console.log ('Não são maior que 3')
+}
