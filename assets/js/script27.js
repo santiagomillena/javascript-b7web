1a91d78748f9b56cd4439288bf005dac2da17a6c
santiagomillena <santiagomillena@gmail.com>
2024-11-29 16:53:04 -0400

    Iteração de array 1

---
+++ assets/js/script27.js
@@ -0,0 +1,8 @@
+let fruits = ['Banana','Laranja','Maça','Pera'];
+
+let bigFruits = fruits.filter((item) => {
+    return item.length > 4;
+})
+
+//let bigFruits = fruits.filter((item) => item.length > 4)
+console.log (bigFruits)
