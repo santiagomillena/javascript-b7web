444c4f73e00e7d6f0fcd98ff20459a6e3f9563f7
santiagomillena <santiagomillena@gmail.com>
2024-11-30 15:17:41 -0400

    createElement/appendChild

---
+++ assets/js/default.js
@@ -0,0 +1,41 @@
+document.getElementById('inputDados').addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') { // Verifica se a tecla pressionada é "Enter"
+    verificar(); // Chama a função "verificar"
+    }
+    
+    });
+    function verificar(){
+        var group = document.getElementById("inputDados").value
+        var dados = group.value
+        var result = document.getElementById ('result')
+
+        if (document.getElementById('inputDados').value == ("")) {
+        alert('Por favor, insira algum dado')
+        return
+        } else {
+            var img = document.createElement('img')
+            img.setAttribute('id', 'photo')
+         if (group == 'aespa') {
+            result.innerHTML = '<p>Você é um MY!<p>'
+            img.setAttribute ('src', 'imagens/aespa250px.png')
+        } else if (group == 'blackpink') {
+            result.innerHTML = '<p>Você é um BLINK!</p>'
+            img.setAttribute ('src', 'imagens/blackpink250px.png')
+        } else if (group == 'itzy'){
+            result.innerHTML = '<P>Você é um MIDZY!</P>'
+            img.setAttribute ('src', 'imagens/itzy250px.png')
+        } else if (group == 'mamamoo'){
+            result.innerHTML = '<P>Você é um MOOMOO!</P>'
+            img.setAttribute ('src', 'imagens/mamamoo250px.png')
+        } else {
+            result.innerHTML = '<p>Desculpe, grupo não reconhecido.</p>';
+            img.setAttribute ('src', 'imagens/interrogacao.png')
+        }
+        result.appendChild(img)
+        document.getElementById('inputDados').value = ""
+    }
+}
+    function customReset (){
+        document.getElementById('inputDados').value = ""
+        document.getElementById('result').innerHTML = ""
+    }
--- assets/js/script29.js
+++ assets/js/script29.js
@@ -9,6 +9,18 @@ function clicou () {
 
     ul.appendChild(newLi)
 }
+// Em um documento HTML, o método document.createElement() cria o elemento HTML especificado por tagName,tagName: É o nome da tag HTML do elemento que você quer criar. Por exemplo: "div", "p", "img", etc.
+//Ex: 
+
+//   var img = document.createElement('img')
+//   img.setAttribute('id', 'photo')
+//   if (group == 'aespa') {
+//   result.innerHTML = '<p>Você é um MY!<p>'
+//   img.setAttribute ('src', 'imagens/aespa250px.png')
+//   }
+//   result.appendChild(img)
+
+
 //  ul.appendChild(newLi)
 // parentElement.appendChild(childElement);
 
@@ -18,3 +30,5 @@ function clicou () {
 //  newLi
 // appendChild adiciona um elemento DOM como último filho de outro elemento.
 // É ideal para criar e manipular elementos dinamicamente na página.
+
+//adicicionando um elemento no final do conteudo que já tem
