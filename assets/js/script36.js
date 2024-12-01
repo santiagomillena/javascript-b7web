25240f1c86e2106b7b6fdf75ae1dee3b3ea36b17
santiagomillena <santiagomillena@gmail.com>
2024-12-01 16:39:53 -0400

    action

--- assets/js/script36.js
+++ assets/js/script36.js
@@ -1,7 +1,20 @@
 class Person {
-    constructor(name, age) {
+
+    age = 0
+    steps = 0
+
+
+    constructor(name) {
         this.name = name
-        this.age = age
+    }
+
+    takeAStep(){
+         this.steps++
+    }
+    setAge(newAge){
+        if (typeof newAge == 'number'){
+            this.age == newAge
+        }
     }
 }
 
@@ -9,4 +22,13 @@ let p1 = new Person('João', 20)
 let p2 = new Person('Maria', 30)
 let p3 = new Person('Pedro', 40)
 
-console.log(p1.name)
+p1.newAge(20)
+
+p1.takeAStep()
+console.log(`Passos de ${p1.name}: ${p1.steps}`)
+
+// p1.age = 20
+
+// console.log(`P1 = ${p1.name} tem ${p1.age} anos`)
+// console.log(`P2 = ${p2.name} tem ${p2.age} anos`)
+// console.log(`P3 = ${p3.name} tem ${p3.age} anos`)
