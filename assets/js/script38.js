2511dd56869b2ce0ada2c9aa4ecf2888f0ace477
santiagomillena <santiagomillena@gmail.com>
2024-12-01 17:40:04 -0400

    static

---
+++ assets/js/script38.js
@@ -0,0 +1,14 @@
+class Person {
+    static hands = 2
+    age = 0
+
+    constructor(name) {
+        this.name = name
+    }
+    sayHi(){
+        console.log(`Oi, eu sou ${this.name} e tenho ${Person.hands} mãos`)
+    }
+}
+
+let p1 = new Person ('Croft')
+p1.sayHi()
