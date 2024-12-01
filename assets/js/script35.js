f64588913f85260f804a5e6ab9750e2117a41c55
santiagomillena <santiagomillena@gmail.com>
2024-12-01 16:03:55 -0400

    constructor, this e instancia

---
+++ assets/js/script35.js
@@ -0,0 +1,15 @@
+class Person {
+    //age = 0
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+}
+
+let p1 = new Person('João', 20)
+let p2 = new Person('Maria', 30)
+let p3 = new Person('Pedro', 40)
+
+//p1.age = 20
+
+console.log(p1.name)
