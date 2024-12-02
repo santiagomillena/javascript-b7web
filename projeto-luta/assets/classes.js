0e2009352fa31a227b8633ffda4399be996ee441
santiagomillena <santiagomillena@gmail.com>
2024-12-02 11:28:38 -0400

    att projeto classes

--- projeto-luta/assets/classes.js
+++ projeto-luta/assets/classes.js
@@ -1,5 +1,5 @@
 // Knight ou Sorcerer
-// LittleMonster ou Bigmonster
+// LittleMonster ou BigMonster
 
 class Character {
 
@@ -30,12 +30,52 @@ class Knight extends Character {
     }
 }
 
-class Sorcerer extends Character{
+class Sorcerer extends Character {
     constructor(name){
         super(name)
         this.life = 80
         this.attack = 15
         this.defense = 3
         this.maxLife = this.life
+    }
+}
+
+class LittleMonster extends Character {
+    constructor(){
+        super('Little Monster')
+        this.life = 40
+        this.attack = 4
+        this.defense = 4
+        this.maxLife = this.life
+    }
+}
+
+class BigMonster extends Character {
+    constructor (){
+        super ('Big Monster')
+        this.life = 120
+        this.attack = 16
+        this.defense = 6
+        this.maxLife = this.life
+    }
+}
+
+class Stage {
+    constructor(fighter1, fighter2, fighter1El, fighter2El){
+        this.fighter1 = fighter1
+        this.fighter2 = fighter2
+        this.fighter1El = fighter1El
+        this.fighter2El = fighter2El
+    }
+    start(){
+        this.update()
+        // TODO: Evento do botão atacar
+    }
+    update(){
+        //Fighter 1
+        this.fighter1El.querySelector('.name').innerHTML = this.fighter1.name
+        let f1Pct = (this.fighter1.life /this.fighter1.maxLife)
+        //Fighter 2
+        this.fighter2El.querySelector('.name').innerHTML = this.fighter2.name
     }
 }
--- projeto-luta/assets/script.js
+++ projeto-luta/assets/script.js
@@ -1,4 +1,10 @@
 let char = new Knight('Croft');
-console.log(char.name)
-console.log(char.life)
-console.log(char.attack)
+let monster = new BigMonster();
+
+let stage = new Stage(
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster')
+)
+stage.start();
