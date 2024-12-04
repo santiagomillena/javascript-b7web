d2c7b6df195a493ff942ff1368e6b16f9539217c
santiagomillena <santiagomillena@gmail.com>
2024-12-04 15:31:06 -0400

    att etapas

--- projeto-urna/script.js
+++ projeto-urna/script.js
@@ -4,3 +4,29 @@ let descricao = document.querySelector('.d-1-4')
 let aviso = document.querySelector('.d-2')
 let lateral = document.querySelector('.d-1-right')
 let numeros = document.querySelector('d-1-3')
+
+let etapaAtual = 0
+
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual]
+
+    seuVotoPara.computedStyleMap.display = 'none'
+    cargo.innerHTML = etapa.titulo
+    descricao.innerHTML = ''
+    aviso.style.display = 'none'
+}
+
+function clicou(n) {
+    alert ('Clicou em '+n)
+}
+function branco() {
+    alert ('Clicou em BRANCO')
+}
+
+function corrige() {
+    alert ('Clicou em CORRIGE')
+}
+
+function confirma() {
+    alert ('Clicou em CONFIRMA')
+}
